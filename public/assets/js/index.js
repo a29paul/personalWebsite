714d750d8a883fcfb2f88a256ab1b3c31a10d159
Aaron Paul <a29paul@uwaterloo.ca>
2018-12-30 23:22:48 -0500

    add button to reveal

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,15 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $("#mainNavbar");
-        var $navLinks = $(".nav-link");
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
 $("#fadein").delay(2000).fadeIn(1000)
+
+$(".revealer").on("click", function () {
+    $(".xpDiv").toggleClass("backgroundColor");
+    $(".h1Ldr").toggleClass("displaynone");
+    $(".h1Sft").toggleClass("displaynone");
+    $(".h1Misc").toggleClass("displaynone");
+    $(".Xp").toggleClass("reveal");
+})
